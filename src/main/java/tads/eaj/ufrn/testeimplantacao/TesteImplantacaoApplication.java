9b61d37334ed440791df6524b7d0d788b05853da
Neto25 <euneto2501@gmail.com>
2021-07-02 01:00:41 -0300

    estrutura inicial

---
+++ src/main/java/tads/eaj/ufrn/testeimplantacao/TesteImplantacaoApplication.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.testeimplantacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteImplantacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteImplantacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tads/eaj/ufrn/testeimplantacao/TesteImplantacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.testeimplantacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteImplantacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
